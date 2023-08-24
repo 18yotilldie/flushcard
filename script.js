89691fa9963e74b17c709b5e00a0d33b3a289ceb
18yotilldie <81391629+18yotilldie@users.noreply.github.com>
2023-08-24 21:27:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
 const words = ["サル","キリン","ライオン","ゾウ","ウシ",
 "ゴリラ","ウサギ","ラッコ","ワニ","パンダ",
 "ネコ","カエル","イヌ","クマ","タヌキ",
-"ブタ","ペンギン","バッタ","ヘビ","ウマ",
-"キツネ","チョウ","ニワトリ","カタツムリ","アザラシ",
-"カンガルー","イカ","カブトムシ","コアラ","タコ",
-"クジラ","ネズミ","カマキリ","ザリガニ","サメ",
-"カニ","クワガタ","ムササビ","クラゲ","カメ",
-"ヤギ","シマウマ","カバ","ヒツジ","サイ",
-"シカ","イノシシ","アライグマ","リス","ラクダ",
-];
+// ... 他の単語 ...
+"シカ","イノシシ","アライグマ","リス","ラクダ"];
 
-var a = words.length;
-while (a) {
-  var j = Math.floor( Math.random() * a );
-  var t = words[--a];
-  words[a] = words[j];
-  words[j] = t;
-}
- 
+var a = 0; // 単語のインデックスを示す変数
+shuffleArray(words); // 単語をシャッフル
+
 var text = document.getElementById("text");
 var next = document.getElementsByClassName("next");
 var nexts = Array.from(next);
+
+// 各ボタンのクリックイベントを設定
 nexts.forEach(function(change){
   change.addEventListener("click",() => {
+    if (a < words.length) {
       text.textContent = words[a];
       count_start();
       a++;
-    })  
+    } else {
+      text.textContent = "おしまい"; // すべての単語を表示した場合のメッセージ
+    }
+  })  
 });
 
-var p = 1;
+var p = 0; // ポイントの変数
+
 document.getElementById("correct")?.addEventListener("click", () => {
-    point.textContent = p;
-    p++;
-  });
+  point.textContent = ++p; // ポイントをインクリメントして表示
+});
 
-//変数の定義---------------------------------------------------------------------
-var count   = 60;     //カウントダウンの数字を格納する変数  1分なので60
-var min     = 0;       //残り時間「分」を格納する変数
-var sec     = 0;       //残り時間「秒」を格納する変数
-var start_f = false; 
-var interval; 
+// 変数の定義と初期化-----------------------------------------------------
+var count = 60; // カウントダウンの数字を格納する変数 1分なので60
+var min = 0; // 残り時間「分」を格納する変数
+var sec = 0; // 残り時間「秒」を格納する変数
+var start_f = false;
+var interval;
 
-//004----------スタート開始------------------------------------------------------
+// スタート開始-------------------------------------------------------------
 function count_start(){
   if (start_f === false) {
-    interval = setInterval(count_down,1000);
+    interval = setInterval(count_down, 1000);
     start_f = true;
   }
 }
   
-//005------------カウントダウンの開始---------------------------------------------
+// カウントダウンの開始-----------------------------------------------------
 function count_down(){
-  //006--------------------------------------------------------------------------
   if(count === 1){
-              var display = document.getElementById("default");
-              display.style.color = 'red';
-              display.innerHTML = "TIME UP!";
-              clearInterval(interval);
-  //007-------------------------------------------------------------------------
+    var display = document.getElementById("default");
+    display.style.color = 'red';
+    display.innerHTML = "TIME UP!";
+    clearInterval(interval);
   } else {
-  //008-------------------------------------------------------------------------
-              count--;
-  //009----------Math.floor関数を使って小数点になった分を整数に変換する。---------------
-              min = Math.floor(count / 60);
-  //010------------60秒で割ったあまりが秒となる-------------------------------------
-              sec = count % 60;
-              var count_down = document.getElementById("default");
-              count_down.innerHTML = ("0"+min) +"：" + ("0"+sec).slice(-2);
+    count--;
+    min = Math.floor(count / 60);
+    sec = count % 60;
+    var count_down = document.getElementById("default");
+    count_down.innerHTML = ("0" + min) + "：" + ("0" + sec).slice(-2);
   }
 }
 
-const restart = document.getElementById("restart");
+// 配列をシャッフルする関数------------------------------------------------
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
 
+// リスタートボタンがクリックされたときの処理-----------------------------------
 document.getElementById("restart")?.addEventListener("click", () => {
-  window.location.reload();
+  window.location.reload(); // ページをリロードしてリスタート
 });
-
