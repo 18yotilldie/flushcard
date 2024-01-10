85f211d3100457152c693ae6bbaf5a68f6763b9a
18yotilldie <81391629+18yotilldie@users.noreply.github.com>
2024-01-10 21:49:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,78 @@
-const words = [“サル”,”キリン”,”ライオン”,”ゾウ”,”ウシ”,”ゴリラ”,”ウサギ”,”ラッコ”,”ワニ”,”パンダ”,”ネコ”,”カエル”,”イヌ”,”クマ”,”タヌキ”,”ブタ”,”ペンギン”,”バッタ”,”ヘビ”,”ウマ”,”キツネ”,”チョウ”,”ニワトリ”,”カタツムリ”,”アザラシ”,”カンガルー”,”イカ”,”カブトムシ”,”コアラ”,”タコ”,”クジラ”,”ネズミ”,”カマキリ”,”ザリガニ”,”サメ”,”カニ”,”クワガタ”,”ムササビ”,”クラゲ”,”カメ”,”ヤギ”,”シマウマ”,”カバ”,”ヒツジ”,”サイ”,”シカ”,”イノシシ”,”アライグマ”,”リス”,”ラクダ”];
+const words = ["サル","キリン","ライオン","ゾウ","ウシ",
+"ゴリラ","ウサギ","ラッコ","ワニ","パンダ",
+"ネコ","カエル","イヌ","クマ","タヌキ",
+"ブタ","ペンギン","バッタ","ヘビ","ウマ",
+"キツネ","チョウ","ニワトリ","カタツムリ","アザラシ",
+"カンガルー","イカ","カブトムシ","コアラ","タコ",
+"クジラ","ネズミ","カマキリ","ザリガニ","サメ",
+"カニ","クワガタ","ムササビ","クラゲ","カメ",
+"ヤギ","シマウマ","カバ","ヒツジ","サイ",
+"シカ","イノシシ","アライグマ","リス","ラクダ",
+];
 
 var a = words.length;
 while (a) {
-var j = Math.floor( Math.random() * a );
-var t = words[–a];
-words[a] = words[j];
-words[j] = t;
+  var j = Math.floor( Math.random() * a );
+  var t = words[--a];
+  words[a] = words[j];
+  words[j] = t;
 }
-
-var text = document.getElementById(“text”);
-var next = document.getElementsByClassName(“next”);
+ 
+var text = document.getElementById("text");
+var next = document.getElementsByClassName("next");
 var nexts = Array.from(next);
 nexts.forEach(function(change){
-change.addEventListener(“click”,() => {
-text.textContent = words[a];
-count_start();
-a++;
-})
+  change.addEventListener("click",() => {
+      text.textContent = words[a];
+      count_start();
+      a++;
+    })  
 });
 
-var p=1;
-document.getElementById(“correct”)?.addEventListener(“click”, () => {
-point.textContent = p;
-p++;
-});
+var p = 1;
+document.getElementById("correct")?.addEventListener("click", () => {
+    point.textContent = p;
+    p++;
+  });
 
-var count = 60;var min = 0;
-var sec = 0;
-var start_f = false;
-var interval;
+//変数の定義---------------------------------------------------------------------
+var count   = 60;     //カウントダウンの数字を格納する変数  1分なので60
+var min     = 0;       //残り時間「分」を格納する変数
+var sec     = 0;       //残り時間「秒」を格納する変数
+var start_f = false; 
+var interval; 
 
+//004----------スタート開始------------------------------------------------------
 function count_start(){
-if (start_f === false) {
-interval = setInterval(count_down,1000);
-start_f = true;
+  if (start_f === false) {
+    interval = setInterval(count_down,1000);
+    start_f = true;
+  }
 }
-}
-
+  
+//005------------カウントダウンの開始---------------------------------------------
 function count_down(){
-if(count === 1){
-var display = document.getElementById(“default”);
-display.style.color = ‘red’;
-display.innerHTML = “TIME UP!”;
-clearInterval(interval);
-} else {
-count–;
-min = Math.floor(count / 60);
-sec = count % 60;
-var count_down=document.getElementById(“default”);
-count_down.innerHTML = (“0″+min) +”：” + (“0″+sec).slice(-2);
-}
+  //006--------------------------------------------------------------------------
+  if(count === 1){
+              var display = document.getElementById("default");
+              display.style.color = 'red';
+              display.innerHTML = "TIME UP!";
+              clearInterval(interval);
+  //007-------------------------------------------------------------------------
+  } else {
+  //008-------------------------------------------------------------------------
+              count--;
+  //009----------Math.floor関数を使って小数点になった分を整数に変換する。---------------
+              min = Math.floor(count / 60);
+  //010------------60秒で割ったあまりが秒となる-------------------------------------
+              sec = count % 60;
+              var count_down = document.getElementById("default");
+              count_down.innerHTML = ("0"+min) +"：" + ("0"+sec).slice(-2);
+  }
 }
 
-const restart = document.getElementById(“restart”);
+const restart = document.getElementById("restart");
 
-document.getElementById(“restart”)?.addEventListener(“click”, () =>
-{window.location.reload();
+document.getElementById("restart")?.addEventListener("click", () => {
+  window.location.reload();
 });
